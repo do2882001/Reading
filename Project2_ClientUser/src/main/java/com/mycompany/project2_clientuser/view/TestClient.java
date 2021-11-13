da139ad502895ef51ec6cc478685131edb4eb393
do2882001 <84132830+do2882001@users.noreply.github.com>
2021-11-13 16:10:44 +0700

    Commit

---
+++ Project2_ClientUser/src/main/java/com/mycompany/project2_clientuser/view/TestClient.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.project2_clientuser.view;
+
+
+
+import Admin.InterfaceAdmin.TinhTong;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Do_Do
+ */
+public class TestClient {
+    public static void main(String[] args) {
+
+        try {
+            TinhTong tinhTong = (TinhTong) Naming.lookup("rmi://localhost:7898/tinhtong");
+
+            int tong = tinhTong.tinhTong(344, 444);
+            System.out.println("ket qua: " + tong);
+            
+//            tinhTong.login(username, pass)
+        } catch (NotBoundException | MalformedURLException | RemoteException ex) {
+            Logger.getLogger(TestClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ Project2_Connector/src/main/java/com/mycompany/project2_connector/Model/DTO/BookDTO.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.project2_connector.Model.DTO;
+
+
+import java.sql.Blob;
+import java.time.Year;
+
+/**
+ *
+ * @author Do_Do
+ */
+public class BookDTO {
+
+    private int BookId;
+    private String BookName;
+    private String Type;
+    private Year ReleaseDate;
+    private Blob PosterUrl;
+    private Blob BookUrl;
+    private Blob AvatarUrl;
+    private CategoryDTO Category;
+    private AuthorDTO Author;
+
+    public BookDTO() {
+    }
+
+    public BookDTO(int BookId, String BookName, String Type, Year ReleaseDate, Blob PosterUrl, Blob BookUrl, Blob AvatarUrl, int CategoryId, CategoryDTO category, AuthorDTO Author) {
+        this.BookId = BookId;
+        this.BookName = BookName;
+        this.Type = Type;
+        this.ReleaseDate = ReleaseDate;
+        this.PosterUrl = PosterUrl;
+        this.BookUrl = BookUrl;
+        this.AvatarUrl = AvatarUrl;
+        this.Category = Category;
+        this.Author = Author;
+    }
+
+    public BookDTO(String BookName, String Type, Year ReleaseDate, Blob PosterUrl, Blob BookUrl, Blob AvatarUrl, int CategoryId, CategoryDTO category, AuthorDTO Author) {
+        this.BookName = BookName;
+        this.Type = Type;
+        this.ReleaseDate = ReleaseDate;
+        this.PosterUrl = PosterUrl;
+        this.BookUrl = BookUrl;
+        this.AvatarUrl = AvatarUrl;
+//        this.CategoryId = CategoryId;
+//        this.category = category;
+        this.Author = Author;
+    }
+
+    public int getBookId() {
+        return BookId;
+    }
+
+    public AuthorDTO getAuthor() {
+        return Author;
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public Year getReleaseDate() {
+        return ReleaseDate;
+    }
+
+    public Blob getPosterUrl() {
+        return PosterUrl;
+    }
+
+    public Blob getBookUrl() {
+        return BookUrl;
+    }
+
+    public Blob getAvatarUrl() {
+        return AvatarUrl;
+    }
+
+    public CategoryDTO getCategory() {
+        return Category;
+    }
+
+   
+    public void setBookId(int BookId) {
+        this.BookId = BookId;
+    }
+
+    public void setBookName(String BookName) {
+        this.BookName = BookName;
+    }
+
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    public void setReleaseDate(Year ReleaseDate) {
+        this.ReleaseDate = ReleaseDate;
+    }
+
+    public void setPosterUrl(Blob PosterUrl) {
+        this.PosterUrl = PosterUrl;
+    }
+
+    public void setBookUrl(Blob BookUrl) {
+        this.BookUrl = BookUrl;
+    }
+
+    public void setAvatarUrl(Blob AvatarUrl) {
+        this.AvatarUrl = AvatarUrl;
+    }
+
+    public void setAuthor(AuthorDTO Author) {
+        this.Author = Author;
+    }
+
+    public void setCategory(CategoryDTO Category) {
+        this.Category = Category;
+    }
+
+
+    
+    
+}
